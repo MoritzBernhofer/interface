bb7f713faa4d524f4dc0546afd11230dff14a4db
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2026-01-23 21:06:33 +0100

    update

---
+++ central_server/Api/Migrations/20260123193630_Initial.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace central_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IotDeviceLogs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<long>(type: "INTEGER", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    IotDeviceId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IotDeviceLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IotDevices",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Ipv4 = table.Column<string>(type: "TEXT", nullable: false),
+                    IotServiceId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IotDevices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IotServices",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IotServices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLogs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<long>(type: "INTEGER", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientServers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<long>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientServers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClientServers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientServers_UserId",
+                table: "ClientServers",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientServers");
+
+            migrationBuilder.DropTable(
+                name: "IotDeviceLogs");
+
+            migrationBuilder.DropTable(
+                name: "IotDevices");
+
+            migrationBuilder.DropTable(
+                name: "IotServices");
+
+            migrationBuilder.DropTable(
+                name: "UserLogs");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- central_server/Api/Services/DatabaseService/AppDataContext.cs
+++ central_server/Api/Services/DatabaseService/AppDataContext.cs
@@ -5,6 +5,14 @@ namespace central_server.Services.DatabaseService;
 
 public class AppDataContext(DbContextOptions<AppDataContext> options) : DbContext(options)
 {
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserLog> UserLogs { get; set; }
+
+    public DbSet<IotService> IotServices { get; set; }
+    public DbSet<IotDeviceLog> IotDeviceLogs { get; set; }
+    public DbSet<IotDevice> IotDevices { get; set; }
+
+    public DbSet<ClientServer> ClientServers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
